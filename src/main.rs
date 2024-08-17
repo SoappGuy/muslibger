006045ec770c267a4d5ae296e0512ad373b3a84e
Yehor Sytnyk <github@dev.cuniling.us>
2024-08-17 12:00:18 +0300

    implemented processing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,73 @@
 mod args_parse;
 mod error;
 
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
+
+use colored::Colorize;
 use error::Error;
+use log::{debug, info, warn};
 
 fn main() -> Result<(), error::Error> {
     env_logger::builder().format_timestamp_secs().init();
 
     let args = args_parse::parse()?;
 
+    process_paths(&args.paths_to_process, |path| {
+        // info!("{path:?}");
+        true
+    })?;
+
     Ok(())
+}
+
+fn process_path(path: &Path, call_on_file: fn(&Path) -> bool) -> Result<i32, Error> {
+    let mut processed = 0;
+
+    if path.is_dir() {
+        debug!("processing {}: \t{:?}", "dir".yellow(), path);
+
+        for path in fs::read_dir(path)? {
+            let path = path?.path();
+
+            if path.exists() {
+                if path.is_dir() {
+                    processed += process_path(&path, call_on_file)?;
+                } else {
+                    debug!(
+                        "processing {}:\t{:?}",
+                        "file".blue(),
+                        &path.file_name().unwrap()
+                    );
+
+                    processed += if call_on_file(&path) { 1 } else { 0 };
+                }
+            }
+        }
+    } else {
+        debug!(
+            "skipping nonexisting {}: \t{:?}",
+            "file".blue(),
+            &path.file_name().unwrap()
+        );
+    }
+    Ok(processed)
+}
+
+fn process_paths(paths: &Vec<PathBuf>, call_on_file: fn(&Path) -> bool) -> Result<i32, Error> {
+    info!(
+        "collected {} entries, processing",
+        paths.len().to_string().red()
+    );
+
+    let mut processed = 0;
+
+    for path in paths {
+        processed += process_path(path, call_on_file)?;
+    }
+
+    info!("processed {} entries", processed.to_string().red());
+    Ok(processed)
 }
